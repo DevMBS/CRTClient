b3ea277db10945f9156ae35e709580285549137a
Mikhail Kuznetsov <78100892+DevMBS@users.noreply.github.com>
2022-01-23 15:07:44 +0300

    Add files via upload

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 const multer  = require("multer");
 const server = require("http").createServer(app);
-const io = require("socket.io")(server);
+const io = require("socket.io")(server, {transports: ['websocket']});
 require("pidcrypt/seedrandom");
 const pidCrypt = require("pidcrypt");
 require("pidcrypt/aes_cbc");
