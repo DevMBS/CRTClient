4ba409da2ee90cfb46db9ac257f3921d2b8c1b18
Mikhail Kuznetsov <78100892+DevMBS@users.noreply.github.com>
2022-02-08 22:23:08 +0300

    Add files via upload

---
+++ app/mobile/index.js
@@ -0,0 +1,96 @@
+'use strict';
+document.querySelector('#status').style.color = 'red';
+import * as THREE from '../../assets/three.js';
+import {GLTFLoader} from 'https://threejsfundamentals.org/threejs/resources/threejs/r127/examples/jsm/loaders/GLTFLoader.js';
+const swiper = new Swiper(".container", {
+    grabCursor: true,
+        effect: "creative",
+        creativeEffect: {
+          prev: {
+            shadow: true,
+            translate: ["-120%", 0, -500],
+          },
+          next: {
+            shadow: true,
+            translate: ["120%", 0, -500],
+          },
+        },
+});
+
+//sockets init
+const socket = io();
+const fileReader = new FileReader();
+const uid = localStorage.getItem('uid');
+if(uid == null){
+  location.href = '../../login/';
+}
+else{
+  socket.emit('uid', uid);
+}
+
+const canvas = document.querySelector('#clover3dview');
+const renderer = new THREE.WebGLRenderer({canvas});
+const fov = 75;
+const aspect = 2;
+const near = 0.1;
+const far = 100;
+const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+camera.position.set(0, 0, 0);
+camera.rotation.y = -Math.PI/2;
+const scene = new THREE.Scene();
+const skyColor = 0xffffff;
+const intensity = 0.7;
+const light = new THREE.AmbientLight(skyColor, intensity);
+scene.add(light);
+const light2 = new THREE.DirectionalLight( 0x00ffa6, 1, 100 );
+light2.position.set( -1000, 100, 100 );
+scene.add( light2 );
+const light3 = new THREE.DirectionalLight( 0x5900ff, 1, 100 );
+light3.position.set( -1000, -100, 100 );
+scene.add( light3 );
+renderer.setClearColor( 0x111111, 1);
+const gltfLoader = new GLTFLoader();
+//loading 3d model of clover
+let clover;
+gltfLoader.load('../../assets/Clover4.glb', (gltf) => {
+  clover = gltf.scene;
+  scene.add(clover);
+  clover.position.set(0.35, 0, 0);
+//render
+function resizeRendererToDisplaySize(renderer) {
+  const canvas = renderer.domElement;
+  const width = canvas.clientWidth;
+  const height = canvas.clientHeight;
+  const needResize = canvas.width !== width || canvas.height !== height;
+  if (needResize) {
+    renderer.setSize(width, height, false);
+  }
+  return needResize;
+}
+function render() {
+  if (resizeRendererToDisplaySize(renderer)) {
+    const canvas = renderer.domElement;
+    camera.aspect = canvas.clientWidth / canvas.clientHeight;
+    camera.updateProjectionMatrix();
+  }
+  renderer.render(scene, camera);
+  requestAnimationFrame(render);
+}
+requestAnimationFrame(render);
+
+
+let previousRotation = {x: clover.rotation.x, y: clover.rotation.y, z: clover.rotation.z};
+function checkDisconnection(){
+  if(previousRotation.x == clover.rotation.x && previousRotation.y == clover.rotation.y && previousRotation.z == clover.rotation.z){
+    document.querySelector('#status').innerHTML = 'Disconnected';
+    document.querySelector('#status').style.color = 'red';
+  }
+  previousRotation.x = clover.rotation.x;
+  previousRotation.y = clover.rotation.y;
+  previousRotation.z = clover.rotation.z;
+}
+
+const checkDisconnectionInterval = setInterval(checkDisconnection, 4000);
+
+
+});
