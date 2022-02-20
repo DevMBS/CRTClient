b3cf1df4eaaeb15b0f07b78e5d523556054dafdc
Mikhail Kuznetsov <78100892+DevMBS@users.noreply.github.com>
2022-02-20 16:32:09 +0300

    + comments and descriptions

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
+//initalizing the socket.io clientside library
 const socket = io();
+//register the service worker
 if ('serviceWorker' in navigator) {
     navigator.serviceWorker.register('/mobile/serviceworker.js');
 }
+//close preloader on page load
 window.onload = function(){
+    //GSAP animation (dissolve block)
     TweenMax.to('#loading', 0.7, {opacity: 0});
     TweenMax.to('.pace', 0.7, {opacity: 0});
     setTimeout(function(){document.getElementById('loading').style.display = 'none';document.getElementsByClassName('pace')[0].style.display = 'none';}, 700);
     TweenMax.to('#down', 0.6, {y:"-=20", yoyo:true, repeat:9999});
 }
+
+//set up button 'back to the top of page'
 var smoothJumpUp = function() {
     if (document.body.scrollTop > 0 || document.documentElement.scrollTop > 0) {
         window.scrollBy(0,-50);
@@ -16,7 +22,7 @@ var smoothJumpUp = function() {
 }
 
 window.onscroll = function() {
-  var scrolled = window.pageYOffset || document.documentElement.scrollTop;
+  let scrolled = window.pageYOffset || document.documentElement.scrollTop;
   if (scrolled > 100) {
       document.getElementById('arrowtop').style.display = 'block';
   } else {
@@ -24,12 +30,18 @@ window.onscroll = function() {
   }
 }
 
+//switch to the mobile version on orientation change
 let switchToMobileVersionInterval = setInterval(function(){if(screen.width<screen.height){location.href="/mobile/index.html"}}, 500);
 
+//sign up button onclick
 function signup(){
+    //hide button
     document.querySelector('#signup').style.display = 'none';
+    //set forbidden caracters for the nickname
     const forbchars = '!@#$%^&*(){}[]:;"\'\\/<>?`~№+=';
+    //forbchars counter
     let i = 0;
+    //check presence of the forbidden chars
     function csc(el){
         document.getElementById(el).value.split('').forEach(char => {
             if(forbchars.includes(char)){
@@ -41,6 +53,7 @@ function signup(){
             }
         });
     }
+    //check field completion
     if(document.getElementById('nickname').value == '' || document.getElementById('password').value == ''){
         if(document.getElementById('nickname').value == ''){
             TweenMax.to('#nickname', 0.1, {x:"+=20", yoyo:true, repeat:5});
@@ -52,19 +65,24 @@ function signup(){
         document.querySelector('#signup').style.display = 'block';
     }
     else{
+        //check forb chars
         csc("nickname");
         if(i == 0){
-            socket.emit('signup', {nickname: document.getElementById('nickname').value.toLowerCase(), password: document.getElementById('password').value});
+            //if everything is okay, send sign up request to the server
+            socket.emit('signup', {nickname: document.getElementById('nickname').value.toLowerCase().trim(), password: document.getElementById('password').value});
         }
     }
 }
+//handle sign up response from the server
 socket.on('signupres', (res) => {
+    //if error
     if(res.body == 'error'){
         document.querySelector('#signup').style.display = 'block';
         document.querySelector('#status').innerText = 'A user with this nickname already exist!';
         document.querySelector('#nickname').value = '';
         TweenMax.to('#nickname', 0.08, {x:"+=20", yoyo:true, repeat:5});
     }
+    //if user has been added to the database
     else if(res.body == 'successful'){
         localStorage.setItem('uid', res.uid);
         location.href = './app/desktop/';
