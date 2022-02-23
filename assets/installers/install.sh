92f37a9eaf1aa5ed430c6e06d5db7fb3ff1ce095
Mikhail Kuznetsov <78100892+DevMBS@users.noreply.github.com>
2022-02-23 21:08:35 +0300

    fixed bug with running mavros.py (no modules in root)

--- assets/installers/install.sh
+++ assets/installers/install.sh
@@ -19,9 +19,10 @@ echo -e "\v\033[35mSetting up the Rescue Clover server process manager...\v\033[
 sudo chmod 777 /var/www/CRTClover/
 sudo npm install pm2@latest -g
 echo -e "\v\033[35mStarting server and setting it to autoload...\v\033[0m"
-sudo pm2 start /var/www/CRTClover/server.js
-sudo pm2 startup
-sudo pm2 save
+pm2 start /var/www/CRTClover/server.js
+pm2 startup
+sudo env PATH=$PATH:/usr/local/bin /home/$USER/.npm-global/lib/node_modules/pm2/bin/pm2 startup systemd -u $USER --hp /home/$USER
+pm2 save
 echo -e "\v\033[32mInstallation successfully completed!\v\033[0m"
 else
 echo "Please copy and paste command from the install tutorial correctly!"
