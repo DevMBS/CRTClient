9441b8848826da748beb8b373f45bb8683834ea8
Mikhail Kuznetsov <78100892+DevMBS@users.noreply.github.com>
2022-02-27 00:58:50 +0300

    fixed pm2 startup error, added installation checks

--- assets/installers/install.sh
+++ assets/installers/install.sh
@@ -2,6 +2,8 @@
 
 clear && echo -e "\v\033[32mRescueClover installation started. Make sure you have a stable internet connection.\v\033[0m"
 echo -e "\v\033[35mDownloading the source code from GitHub...\v\033[0m"
+if ! [ -d ./CRTClover/ ]
+then
 git clone https://github.com/DevMBS/CRTClover.git
 echo -e "\v\033[35mInstalling the latest stable version of NodeJS and npm...\v\033[0m"
 sudo npm cache clean -f && sudo npm install -g n && sudo n install stable
@@ -21,9 +23,17 @@ sudo npm install pm2@latest -g
 echo -e "\v\033[35mStarting server and setting it to autoload...\v\033[0m"
 pm2 start /var/www/CRTClover/server.js
 pm2 startup
+if [ -d /home/$USER/.npm-global/ ]
+then
 sudo env PATH=$PATH:/usr/local/bin /home/$USER/.npm-global/lib/node_modules/pm2/bin/pm2 startup systemd -u $USER --hp /home/$USER
+else
+sudo env PATH=$PATH:/usr/local/bin /usr/local/lib/node_modules/pm2/bin/pm2 startup systemd -u $USER --hp /home/$USER
+fi
 pm2 save
 echo -e "\v\033[32mInstallation successfully completed!\v\033[0m"
 else
-echo "Please copy and paste command from the install tutorial correctly!"
+echo "\v\033[31mPlease copy and paste command from the install tutorial correctly!\v\033[0m"
+fi
+else
+echo "\v\033[31mPlease delete all files from previous installations of our software! (sudo rm -r ./CRTClover/)\v\033[0m"
 fi
--- assets/installers/uninstall.sh
+++ assets/installers/uninstall.sh
@@ -1,8 +1,14 @@
 #Clover Rescue Team's official Cloverside software uninstaller
 
-clear && echo -e "\v\033[31mThis program started deleting RescueClover software from your drone. We hope you know what you are doing and understand the consequences :)\v\033[0m"
+clear && echo -e "\v\033[31mThis program started deleting RescueClover software from your drone. Continue? (y/n): \v\033[0m"
+read answer
+if [$answer == 'y'] || [$answer == 'Y']
+then
 pm2 stop /var/www/CRTClover/server.js
 pm2 unstartup
 sudo rm -r /var/www/CRTClover/
-
-echo -e "\v\033[31mYou need to restart your drone to complete uninstalling.\v\033[0m"
+echo 'Complete.'
+elif [$answer == 'n'] || [$answer == 'N']
+then
+echo 'Stopped.'
+fi
