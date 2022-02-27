f0563f4ac59a62f029e4f0c0a42bb5f745c2f6db
Mikhail Kuznetsov <78100892+DevMBS@users.noreply.github.com>
2022-02-27 21:45:30 +0300

    fix pm2 unstartup error

--- assets/installers/uninstall.sh
+++ assets/installers/uninstall.sh
@@ -2,13 +2,19 @@
 
 clear && echo -e "\v\033[31mThis program started deleting RescueClover software from your drone. Continue? (y/n): \v\033[0m"
 read answer
-if [$answer == 'y'] || [$answer == 'Y']
+if [$answer == y]
 then
 pm2 stop /var/www/CRTClover/server.js
 pm2 unstartup
 sudo rm -r /var/www/CRTClover/
+if [ -d /home/$USER/.npm-global/ ]
+then
+sudo env PATH=$PATH:/usr/local/bin /home/$USER/.npm-global/lib/node_modules/pm2/bin/pm2 unstartup systemd -u $USER --hp /home/$USER
+else
+sudo env PATH=$PATH:/usr/local/bin /usr/local/lib/node_modules/pm2/bin/pm2 unstartup systemd -u $USER --hp /home/$USER
+fi
 echo 'Complete.'
-elif [$answer == 'n'] || [$answer == 'N']
+elif [$answer == n]
 then
 echo 'Stopped.'
 fi
