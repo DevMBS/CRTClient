fa8c49181ee4b0b484b508fca4886c005c3cce70
Mikhail Kuznetsov <78100892+DevMBS@users.noreply.github.com>
2022-03-22 19:27:24 +0300

    signin disabled button bug fix; add button ripple

--- login/login.js
+++ login/login.js
@@ -10,6 +10,27 @@ window.onload = function(){
     TweenMax.to('.pace', 0.7, {opacity: 0});
     setTimeout(function(){document.getElementById('loading').style.display = 'none';document.getElementsByClassName('pace')[0].style.display = 'none';}, 700);
 }
+
+//ripple effect for buttons
+let buttons = document.getElementsByTagName('button');
+Array.prototype.forEach.call(buttons, function(b){
+    b.addEventListener('click', ripple);
+})
+function ripple(e)
+{
+    if(this.getElementsByClassName('ripple').length > 0)
+    {
+        this.removeChild(this.childNodes[1]);
+    }
+    let circle = document.createElement('div');
+    this.appendChild(circle);
+    let d = Math.max(this.clientWidth, this.clientHeight);
+    circle.style.width = circle.style.height = d + 'px';
+    circle.style.left = e.clientX - this.offsetLeft - d / 2 + 'px';
+    circle.style.top = e.clientY - this.offsetTop - d / 2 + 'px';
+    circle.classList.add('ripple');
+}
+
 //sign in button onclick
 function signin(){
     //hide button
@@ -57,14 +78,14 @@ document.addEventListener('keydown', (e)=>{
 socket.on('signinres', (res) => {
     //if wrong nickname
     if(res.body == "nickname_error"){
-        document.getElementById('signin').style.display = 'block';
+        document.querySelector('#signin').removeAttribute('disabled');
         document.querySelector('#status').innerText = 'Hmm.. We can\'t find a user with this nickname in our database';
         document.querySelector('#nickname').value = '';
         TweenMax.to('#nickname', 0.08, {x:"+=20", yoyo:true, repeat:5});
     }
     //if wrong password
     else if(res.body == "password_error"){
-        document.getElementById('signin').style.display = 'block';
+        document.querySelector('#signin').removeAttribute('disabled');
         document.querySelector('#status').innerText = 'Incorrect password!';
         document.querySelector('#password').value = '';
         TweenMax.to('#password', 0.08, {x:"+=20", yoyo:true, repeat:5});
